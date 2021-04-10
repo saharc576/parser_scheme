a2d82ddc15a87d9fa1291fa06472002ea2d55a50
sahar <sahar576cohen@gmail.com>
2021-04-10 16:50:39 +0300

    removed js files

--- shared/box.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setBox = exports.unbox = exports.makeBox = void 0;
-const makeBox = (x) => ([x]);
-exports.makeBox = makeBox;
-const unbox = (b) => b[0];
-exports.unbox = unbox;
-const setBox = (b, v) => { b[0] = v; return; };
-exports.setBox = setBox;
