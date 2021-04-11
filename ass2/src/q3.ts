68dbd9476e571d2558c8f8470debc46f554b85ba
sahar <sahar576cohen@gmail.com>
2021-04-11 21:26:08 +0300

    parse to proc not completed

--- ass2/src/q3.ts
+++ ass2/src/q3.ts
@@ -1,13 +1,32 @@
-import { /*ClassExp, ProcExp, */ Exp, Program } from "./L31-ast";
+import { map, reduce } from 'ramda';
+import { ClassExp, ProcExp, Exp, Program } from "./L31-ast";
 import { Result, makeFailure } from "../shared/result";
+import { makeNumExp, makeProcExp, makeVarDecl, makeIfExp, makeStrExp } from "../imp/L3-ast";
+import { second } from '../shared/list';
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
-//export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
+export const class2proc = (exp: ClassExp): ProcExp => {
+    
+    
+    const fields = exp.fields;
+    const methods = exp.methods;
+    const methodsNames = map(m => m.var , methods);
+    const genericStr = "eq? msg '";
+    // const initVal = makeIfExp(makeStrExp(`eq? msg ${methodsNames[0]}`),makeStrExp(`${makeProcExp([], fields[0])}`),makeStrExp(""))
+    const procBody = makeProcExp(fields, [makeProcExp([makeVarDecl("msg")], 
+        reduce((acc, curr) => {acc[0].test; return acc } , 
+        [makeIfExp(makeStrExp(""),makeStrExp(""),makeStrExp(""))], methodsNames )))
+    // makeProcExp()
+    
+
+    return makeProcExp([makeVarDecl("Sdf")], [makeNumExp(1)] )
+}
+   
+
 
 /*
 Purpose: Transform L31 AST to L3 AST
