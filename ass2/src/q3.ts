235938aa00aa0ac2d4a0661b4b32994db2e47ac8
sahar <sahar576cohen@gmail.com>
2021-04-12 19:09:06 +0300

    3b first part done

--- ass2/src/q3.ts
+++ ass2/src/q3.ts
@@ -1,7 +1,7 @@
 import { map, reduce } from 'ramda';
-import { ClassExp, ProcExp, Exp, Program, Binding, isBinding, makeClassExp, isClassExp } from "./L31-ast";
-import { Result, makeFailure } from "../shared/result";
-import { makeNumExp, makeProcExp, makeVarDecl, makeIfExp, makeStrExp, CExp } from "../imp/L3-ast";
+import {  makeNumExp, makeProcExp, makeVarDecl, makeIfExp, makeStrExp, CExp, makePrimOp, ClassExp, ProcExp, Exp, Program, Binding, isBinding, makeClassExp, isClassExp, makeBoolExp, makeAppExp, makeVarRef } from "./L31-ast";
+import { Result, makeFailure, makeOk } from "../shared/result";
+// import { makeNumExp, makeProcExp, makeVarDecl, makeIfExp, makeStrExp, CExp, makePrimOp } from "../imp/L3-ast";
 import { rest, second } from '../shared/list';
 
 /*
@@ -9,16 +9,18 @@ Purpose: Transform ClassExp to ProcExp
 Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
-export const class2proc = (exp: ClassExp): ProcExp => {
+export const class2proc = (exp: ClassExp): ProcExp => 
+     makeProcExp(exp.fields , [makeProcExp([makeVarDecl("msg")], [rewriteMethods(exp.methods)])])
     
-    const tmp = makeProcExp(exp.fields , [makeProcExp([makeVarDecl("msg")], rewriteMethods(exp.methods))])
-    console.log(tmp);
-    return tmp;
-}
 
-const rewriteMethods = (methods: Binding[]): CExp[] => 
-    methods.length === 1 ? [makeStrExp(`(if (eq? msg ${methods[0].val})) ((${methods[0].var})) #f)`)]:
-        [makeStrExp(`(if (eq? msg ${methods[0].val})) ((${methods[0].var})) ${rewriteMethods(rest(methods))}`)]
+const rewriteMethods = (methods: Binding[]): CExp => 
+    methods.length === 1 ? makeIfExp(makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeVarRef(`'${methods[0].var.var}`)]), 
+                                    makeAppExp(methods[0].val, []), 
+                                    makeBoolExp(false)):
+                                    makeIfExp(makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeVarRef(`'${methods[0].var.var}`)]), 
+                                    makeAppExp(methods[0].val, []), 
+                                    rewriteMethods(rest(methods)));
+
 
 
 /*
@@ -26,5 +28,7 @@ Purpose: Transform L31 AST to L3 AST
 Signature: l31ToL3(l31AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
-export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    makeFailure("TODO");
+export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> => 
+    isClassExp(exp) ? makeOk(class2proc(exp)) : makeFailure("TODO");
+
+    
--- ass2/test/q3.test.ts
+++ ass2/test/q3.test.ts
@@ -13,16 +13,18 @@ describe('Q3 Tests', () => {
           expect(bind(p(`(class ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))`),parseL31Exp)).is.satisfy(isFailure);
      });
 
+
      it('test parse/unparse program', () => {
-          expect(bind(parseL31(`(L31 (define pair (class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`), x=> makeFailure("blah")));
+          expect(bind(parseL31(`(L31 (define pair (class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`), x=>makeOk(unparseL31(x)))).to.deep.equal(makeOk(`(L31 (define pair (class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`));
          });
      
-    //  it('trnasform class-exp in to proc-exp', () => {
-    //       expect(bind(bind(bind(p(`(class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))`), parseL31Exp), L31ToL3),  x=>makeOk(unparseL31(x)))).to.deep.equal(makeOk(`(lambda (a b) (lambda (msg) (if (eq? msg 'first) ((lambda () a) ) (if (eq? msg 'second) ((lambda () b) ) (if (eq? msg 'sum) ((lambda () (+ a b)) ) #f)))))`));
-    //  });
+     
+     it('trnasform class-exp in to proc-exp', () => {
+          expect(bind(bind(bind(p(`(class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))`), parseL31Exp), L31ToL3),  x=>makeOk(unparseL31(x)))).to.deep.equal(makeOk(`(lambda (a b) (lambda (msg) (if (eq? msg 'first) ((lambda () a) ) (if (eq? msg 'second) ((lambda () b) ) (if (eq? msg 'sum) ((lambda () (+ a b)) ) #f)))))`));
+     });
 
-    //  it('trnasform class-exp program in to proc-exp', () => {
-    //       expect(bind(bind(parseL31(`(L31 (define pair (class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`), L31ToL3),  x=>makeOk(unparseL31(x)))).to.deep.equal(makeOk(`(L31 (define pair (lambda (a b) (lambda (msg) (if (eq? msg 'first) ((lambda () a) ) (if (eq? msg 'second) ((lambda () b) ) (if (eq? msg 'sum) ((lambda () (+ a b)) ) #f)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`));
-    //  });
+     // it('trnasform class-exp program in to proc-exp', () => {
+     //      expect(bind(bind(parseL31(`(L31 (define pair (class (a b) ((first (lambda () a)) (second (lambda () b)) (sum (lambda () (+ a b)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`), L31ToL3),  x=>makeOk(unparseL31(x)))).to.deep.equal(makeOk(`(L31 (define pair (lambda (a b) (lambda (msg) (if (eq? msg 'first) ((lambda () a) ) (if (eq? msg 'second) ((lambda () b) ) (if (eq? msg 'sum) ((lambda () (+ a b)) ) #f)))))) (let ((p12 (pair 1 2)) (p34 (pair 3 4))) (if (> (p12 'first) (p34 'second)) #t #f)))`));
+     // });
      
 });
